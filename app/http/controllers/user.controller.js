5b4a572584dfc6a5ee4248321cc4bab398e630e2
mohammadprv <mohammadpeyravi11@gmail.com>
2022-11-06 17:49:11 +0330

    Create Update profile

--- app/http/controllers/user.controller.js
+++ app/http/controllers/user.controller.js
@@ -1,8 +1,9 @@
+const { UserModel } = require("../../models/user");
+
 class UserController {
 
     getProfile(req, res, next) {
         try {
-            
             const user = req.user;
             res.status(200).json({
                 status: 200,
@@ -15,8 +16,32 @@ class UserController {
         }
     }
 
-    editProfile() {
+    async editProfile(req, res, next) {
+        try {
 
+            let data = req.body;
+            const userID = req.user._id;
+            const fields = ["first_name", "last_name", "skills"];
+            const badValues = ["", " ", undefined, NaN, null, 0, -1, [], {}];
+
+            Object.entries(data).forEach(([key, value]) => {
+                if(!fields.includes(key)) delete data[key];
+                if(badValues.includes(value)) delete data[key];
+            })
+
+            const result = await UserModel.updateOne({ _id: userID }, { $set: data });
+            if(result.modifiedCount > 0) {
+                return res.status(200).json({
+                    status: 200,
+                    success: true,
+                    message: "به‌روزرسانی پروفایل شما با موفقیت انجام شد"
+                });
+            }
+            throw { status: 400, success: false, message: "به‌روزرسانی پروفایل انجام نشد" };
+
+        } catch (error) {
+            next(error)
+        }
     }
 
     addSkills() {
--- app/routes/user.js
+++ app/routes/user.js
@@ -7,6 +7,8 @@ const router = require('express').Router();
 //? User Profile Route
 router.get("/profile", checkLogin, UserController.getProfile);
 
+//? Edit User Profile Route
+router.post("/profile", checkLogin, UserController.editProfile);
 
 module.exports = {
     userRoutes: router
